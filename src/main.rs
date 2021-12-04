92dea6d0af565fff31fa4f577c02796cd7408cd1
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-04 16:53:17 -0600

    Get mpris metadata

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// Standard library imports
+
+
+// External imports
+use mpris::PlayerFinder;
+
 fn main() {
-    println!("Hello, world!");
+    let player = PlayerFinder::new().expect("Error connecting to dbus")
+                                    .find_active().expect("Not playing");
+
+    let metadata = player.get_metadata()
+        .expect("Unable to get player metadata");
+
+    println!("{:#?}", metadata);
+
 }
