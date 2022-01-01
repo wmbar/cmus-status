23d6193e815285adb5d56813760611e5edbf6871
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-31 19:12:10 -0600

    Working widget

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 // External imports
-use mpris::{PlayerFinder, Player, Metadata};
-
+use mpris::{Metadata, Player, PlayerFinder};
 
 fn main() {
-    let player: Player = PlayerFinder::new().expect("Error connecting to dbus")
-                                    .find_active().expect("Not playing");
+    let player: Player = PlayerFinder::new()
+        .expect("Error connecting to dbus")
+        .find_active()
+        .expect("Not playing");
 
-    let metadata: Metadata = player.get_metadata()
+    let metadata: Metadata = player
+        .get_metadata()
         .expect("Unable to get player metadata");
 
-    println!("{:#?} - {:#?}", metadata.title(), metadata.artists())
+    let song_title = match metadata.title() {
+        Some(r) => r.to_string(),
+        None => "No song found!".to_string(),
+    };
+
+    let artist_name = match metadata.artists() {
+        Some(r) => r[0].to_string(),
+        None => "No artist found!".to_string(),
+    };
+
+    println!("{} by {}", song_title, artist_name)
 }
